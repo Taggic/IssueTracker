8e0f390a8b437f2fa571cf2d777b0d752b0b5eb7
Taggic <taggic@t-online.de>
2012-11-14 22:34:31 +0100

    Signed-off-by: Taggic <taggic@t-online.de>

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,54 +1,53 @@
 <?php
 /**
- * Arquivo de linguagem em Portugu�s do Brasil para IssueTracker
+ * Arquivo de linguagem em Português do Brasil para IssueTracker
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Benedito Carneiro <ctsbc@yahoo.com.br>
  */
 /******************************************************************************/
 // Issue Report controls
-$lang['msg_reporttrue'] = 'Seu relat&oacute;rio foi armazenado com sucesso como ocorr&ecirc;ncia #';
-$lang['wmsg1'] = 'Informe um endere&ccedil;o de email v&aacute;lido, de prefer&ecirc;ncia o seu, para esclarecimentos e/ou retorno sobre a ocorr&ecirc;ncia relatada.';
-$lang['wmsg2'] = 'Informe uma vers&atilde;o de produto v&aacute;lida para relatar adequadamente esta ocorr&ecirc;ncia.';
-$lang['wmsg3'] = 'Informe uma descri&ccedil;&atilde;o melhor da sua ocorr&ecirc;ncia.';
-$lang['wmsg4'] = '&nbsp;Por favor, <a href="?do=login&amp" class="action login" accesskey="" rel="nofollow" style="color:blue;text-decoration:underline;" title="Autentica&ccedil;&atilde;o">Autentique-se</a> se quer relatar uma ocorr&ecirc;ncia.';
-$lang['wmsg5'] = 'Informe um t&iacute;tulo adequadamente descritivo para a ocorr&ecirc;ncia.';
-$lang['wmsg6'] = 'Erro de Upload: O tamanho m&aacute;ximo do arquivo (%s bytes) foi ultrapassado.';
-$lang['wmsg7'] = 'Erro de Upload: A extens&atilde;o do arquivo &eacute; inv&aacute;lida.';
-$lang['wmsg8'] = 'Erro de Upload: O Mime-Type do arquivo n&atilde;o existe ou n&atilde;o &eacute; suportado.';
-$lang['wmsg9'] = 'Seguran&ccedil;a Antispam: O upload do seu IP est&aacute; bloqueado por mais %s minutos, pelo menos.';
+$lang['msg_reporttrue'] = 'Seu relatório foi armazenado com sucesso como ocorrência #';
+$lang['wmsg1'] = 'Informe um endereço de email válido, de preferência o seu, para esclarecimentos e/ou retorno sobre a ocorrência relatada.';
+$lang['wmsg2'] = 'Informe uma versão de produto válida para relatar adequadamente esta ocorrência.';
+$lang['wmsg3'] = 'Informe uma descrição melhor da sua ocorrência.';
+$lang['wmsg4'] = '&nbsp;Por favor, <a href="?do=login&amp" class="action login" accesskey="" rel="nofollow" style="color:blue;text-decoration:underline;" title="Autenticação">Autentique-se</a> se quer relatar uma ocorrência.';
+$lang['wmsg5'] = 'Informe um título adequadamente descritivo para a ocorrência.';
+$lang['wmsg6'] = 'Erro de Upload: O tamanho máximo do arquivo (%s bytes) foi ultrapassado.';
+$lang['wmsg7'] = 'Erro de Upload: A extensão do arquivo é inválida.';
+$lang['wmsg8'] = 'Erro de Upload: O Mime-Type do arquivo não existe ou não é suportado.';
+$lang['wmsg9'] = 'Segurança Antispam: O upload do seu IP está bloqueado por mais %s minutos, pelo menos.';
 $lang['lbl_symptomupload'] = 'Upload de arquivo de sintomas:';
 $lang['btn_reportsave'] = 'Enviar';
 // further settings see 'th_...' options at Issue List controls section below 
 
 /******************************************************************************/
 // Issue List controls
-$lang['lbl_issueqty']           = 'Quantidade de %s ocorr&ecirc;ncias:&nbsp;';
-$lang['lbl_scroll']             = 'Rolar a lista de ocorr&ecirc;ncias: &nbsp;&nbsp;&nbsp;';
+$lang['lbl_issueqty']           = 'Quantidade de %s ocorrências:&nbsp;';
+$lang['lbl_scroll']             = 'Rolar a lista de ocorrências: &nbsp;&nbsp;&nbsp;';
 $lang['lbl_filtersev']          = 'Filtrar Severidade:&nbsp;';
 $lang['lbl_filterstat']         = 'Filtrar Status:&nbsp;';
 $lang['lbl_filterprod']         = 'Filtrar Produto:&nbsp;';
-$lang['cbx_myissues']           = 'Meus incidentes:&nbsp;';
 $lang['btn_go']                 = 'OK';
 
 $lang['btn_previuos']           = '<<<';
-$lang['btn_previuos_title']     = 'Ocorr&ecirc;ncias anteriores';
+$lang['btn_previuos_title']     = 'Ocorrências anteriores';
 $lang['btn_next']               = '>>>';
-$lang['btn_next_title']         = 'pr�ximas Ocorr&ecirc;ncias';
+$lang['btn_next_title']         = 'próximas Ocorrências';
 
-$lang['lbl_showid']             = 'Mostrar detalhes da ocorr&ecirc;ncia:';
+$lang['lbl_showid']             = 'Mostrar detalhes da ocorrência:';
 $lang['btn_showid']             = 'Mostrar';
 $lang['btn_showid_title']       = 'Mostrar';
 
-$lang['msg_commentfalse']       = 'Este coment&aacute;rio j&aacute; existe e n&atilde;o foi adicionado novamente.';
-$lang['msg_commenttrue']        = 'Seu coment&aacute;rio foi armazenado com sucesso com ID #';
-$lang['msg_commentmodtrue']     = 'Seu coment&aacute;rio foi modificado com sucesso com ID #';
-$lang['msg_commentdeltrue']     = 'Seu coment&aacute;rio #%s foi exclu&iacute;do com sucesso.';
-$lang['msg_commentmodfalse']    = 'N&atilde;o tem diferen&ccedil;a do coment&aacute;rio ID #';
-$lang['msg_pfilemissing']       = 'O arquivo do projeto n&atilde;o existe: %s .ocorr&ecirc;ncias. ';
-$lang['msg_issuemissing']       = 'N&atilde;o existe ocorr&ecirc;ncia com ID ';
+$lang['msg_commentfalse']       = 'Este comentário já existe e não foi adicionado novamente.';
+$lang['msg_commenttrue']        = 'Seu comentário foi armazenado com sucesso com ID #';
+$lang['msg_commentmodtrue']     = 'Seu comentário foi modificado com sucesso com ID #';
+$lang['msg_commentdeltrue']     = 'Seu comentário #%s foi excluído com sucesso.';
+$lang['msg_commentmodfalse']    = 'Não tem diferença do comentário ID #';
+$lang['msg_pfilemissing']       = 'O arquivo do projeto não existe: %s .ocorrências. ';
+$lang['msg_issuemissing']       = 'Não existe ocorrência com ID ';
 $lang['msg_captchawrong']       = 'Resposta errada para a pergunta antispam.';
-$lang['msg_descrmodtrue']       = 'Descri&ccedil;&atilde;o modificada com sucesso.';
+$lang['msg_descrmodtrue']       = 'Descrição modificada com sucesso.';
 $lang['msg_slinkmodtrue']       = 'Links do sintoma modificados com sucesso.';
 $lang['msg_severitymodtrue']    = 'Severidade modificado com sucesso.';
 $lang['msg_statusmodtrue']      = 'Status modificado com sucesso.';
@@ -61,105 +60,105 @@ $lang['lbl_project']            = 'Projeto:';
 $lang['lbl_issueid']            = 'ID:';
 $lang['lbl_reporter']           = 'Relatado por:';
 $lang['lbl_reporterdtls']       = 'Detalhes do Relator';
-$lang['lbl_initdescr']          = 'Descri&ccedil;&atilde;o inicial';
+$lang['lbl_initdescr']          = 'Descrição inicial';
 $lang['lbl_reportername']       = 'Nome:';
 $lang['lbl_reportermail']       = 'Email:';
 $lang['lbl_reporterphone']      = 'Telefone:';
 $lang['lbl_reporteradcontact']  = 'Adicionar contato:';
 $lang['lbl_symptlinks']         = 'Links para arquivos de sintomas';
-$lang['lbl_cmts_wlog']          = 'Coment&aacute;rios (registro de trabalho)';
-$lang['lbl_cmts_adcmt']         = 'Adicionar um novo coment&aacute;rio';
-$lang['lbl_cmts_edtres']        = 'Solu&ccedil;&atilde;o';
+$lang['lbl_cmts_wlog']          = 'Comentários (registro de trabalho)';
+$lang['lbl_cmts_adcmt']         = 'Adicionar um novo comentário';
+$lang['lbl_cmts_edtres']        = 'Solução';
 $lang['btn_add']                = 'Adicionar';
 $lang['btn_add_title']          = 'Adicionar';
 $lang['btn_mod']                = 'Atualizar'; // to submit comment modification
 $lang['btn_mod_title']          = 'Atualizar';
-$lang['del_title']              = 'Excluir este coment&aacute;rio';
-$lang['lbl_signin']             = 'Autentique-se</a> se quer adicionar um coment&aacute;rio ou nota de solu&ccedil;&atilde;o.'; // </a> necessary to close the link tag
+$lang['del_title']              = 'Excluir este comentário';
+$lang['lbl_signin']             = 'Autentique-se</a> se quer adicionar um comentário ou nota de solução.'; // </a> necessary to close the link tag
 $lang['lbl_please']             = 'Por favor ';
-$lang['lbl_lessPermission']     = 'Seu n&iacute;vel de permiss&atilde;o &eacute; muito baixo. Contate o administrador do wiki.';
+$lang['lbl_lessPermission']     = 'Seu nível de permissão é muito baixo. Contate o administrador do wiki.';
 
 $lang['th_project']             = 'Projeto';
 $lang['th_id']                  = 'Id';
 $lang['th_created']             = 'Criado';
 $lang['th_product']             = 'Produto';
-$lang['th_version']             = 'Vers&atilde;o';
+$lang['th_version']             = 'Versão';
 $lang['th_severity']            = 'Severidade';
 $lang['th_status']              = 'Status';
-$lang['th_username']            = 'Nome do usu&aacute;rio';
-$lang['th_usermail']            = 'Email do usu&aacute;rio';
-$lang['th_userphone']           = 'Telefone do usu&aacute;rio';
+$lang['th_username']            = 'Nome do usuário';
+$lang['th_usermail']            = 'Email do usuário';
+$lang['th_userphone']           = 'Telefone do usuário';
 $lang['th_reporteradcontact']   = 'Adicionar contato';
-$lang['th_title']               = 'T&iacute;tulo';
-$lang['th_description']         = 'Descri&ccedil;&atilde;o da Ocorr&ecirc;ncia';
+$lang['th_title']               = 'Título';
+$lang['th_description']         = 'Descrição da Ocorrência';
 $lang['th_sympt']               = 'Link do sintoma ';
-$lang['th_assigned']            = 'Atribu&iacute;do a'; 
-$lang['th_resolution']          = 'Solu&ccedil;&atilde;o';
+$lang['th_assigned']            = 'Atribuído a'; 
+$lang['th_resolution']          = 'Solução';
 $lang['th_modified']            = 'Modificado';
-$lang['th_showmodlog']          = 'Hist&oacute;rico de Status';
-$lang['h_modlog']               = 'Hist&oacute;rico de modifica&ccedil;&atilde;o do Status #';
-$lang['mod_valempty']           = '[exclu&iacute;do]';
+$lang['th_showmodlog']          = 'Histórico de Status';
+$lang['h_modlog']               = 'Histórico de modificação do Status #';
+$lang['mod_valempty']           = '[excluído]';
 $lang['back']                   = 'voltar';
 $lang['gen_tab_open']           = 'Detalhes';
 $lang['descr_tab_mod']          = 'Modificar';
-$lang['cmt_tab_open']           = 'adicionar Coment&aacute;rio';
-$lang['cmt_tab_mod']            = 'modificar Coment&aacute;rio';
-$lang['rsl_tab_open']           = 'adicionar / modificar Solu&ccedil;&atilde;o';
-$lang['dtls_usr_hidden']        = 'detalhes do usu&aacute;rio escondidos';
-$lang['minor_mod']              = 'Altera&ccedil;&atilde;o menor';
-$lang['minor_mod_cbx_title']    = 'evitar enviar emails depois de altera&ccedil;&otilde;es cosm&eacute;ticas';
+$lang['cmt_tab_open']           = 'adicionar Comentário';
+$lang['cmt_tab_mod']            = 'modificar Comentário';
+$lang['rsl_tab_open']           = 'adicionar / modificar Solução';
+$lang['dtls_usr_hidden']        = 'detalhes do usuário escondidos';
+$lang['minor_mod']              = 'Alteração menor';
+$lang['minor_mod_cbx_title']    = 'evitar enviar emails depois de alterações cosméticas';
 /******************************************************************************/
 /* send an e-mail to user due to issue modificaion
 /* _emailForIssueMod
 */                            
-$lang['cmnt_new_subject']       = 'Ocorr&ecirc;ncia #%s em %s: novo coment&aacute;rio adicionado'; // $issue['id'], $project
-$lang['cmnt_mod_subject']       = 'Ocorr&ecirc;ncia #%s em %s: coment&aacute;rio modificado';  // $issue['id'], $project
-$lang['cmnt_del_subject']       = 'Ocorr&ecirc;ncia #%s on %s: coment&aacute;rio exclu&iacute;do';   // $issue['id'], $project
-$lang['cmt_del_intro']          = 'Um coment&aacute;rio foi exclu&iacute;do.';
-$lang['issuemod_subject']       = 'Ocorr&ecirc;ncia #%s em %s: %s'; //$issue['id'], $project, $column
-$lang['issuemod_head']          = 'Caro usu&aacute;rio,';
-$lang['issuemod_intro']         = 'A ocorr&ecirc;ncia relatada foi modificada.';
+$lang['cmnt_new_subject']       = 'Ocorrência #%s em %s: novo comentário adicionado'; // $issue['id'], $project
+$lang['cmnt_mod_subject']       = 'Ocorrência #%s em %s: comentário modificado';  // $issue['id'], $project
+$lang['cmnt_del_subject']       = 'Ocorrência #%s on %s: comentário excluído';   // $issue['id'], $project
+$lang['cmt_del_intro']          = 'Um comentário foi excluído.';
+$lang['issuemod_subject']       = 'Ocorrência #%s em %s: %s'; //$issue['id'], $project, $column
+$lang['issuemod_head']          = 'Caro usuário,';
+$lang['issuemod_intro']         = 'A ocorrência relatada foi modificada.';
 $lang['issuemod_issueid']       = 'ID:     ';
 $lang['issuemod_status']        = 'Status:   ';
 $lang['issuemod_product']       = 'Produto:   ';
-$lang['issuemod_version']       = 'Vers&atilde;o:   ';
+$lang['issuemod_version']       = 'Versão:   ';
 $lang['issuemod_severity']      = 'Severidade:   ';
 $lang['issuemod_creator']       = 'Criador:   ';
-$lang['issuemod_title']         = 'T&iacute;tulo:   ';
-$lang['issuemod_cmntauthor']    = 'Coment&aacute;rio por:  ';
+$lang['issuemod_title']         = 'Título:   ';
+$lang['issuemod_cmntauthor']    = 'Comentário por:  ';
 $lang['issuemod_date']          = 'submetido em: ';
-$lang['issuemod_cmnt']          = 'Coment&aacute;rio:  ';
+$lang['issuemod_cmnt']          = 'Comentário:  ';
 $lang['issuemod_see']           = 'veja detalhes:  ';
 $lang['issuemod_br']            = 'atenciosamente';
-$lang['issuemod_end']           = ' Analisador de Ocorr&ecirc;ncias';    // project name placed before this
-$lang['issuedescrmod_subject']  = 'Ocorr&ecirc;ncia #%s em %s: descri&ccedil;&atilde;o inicial modificada'; // $issue['id'], $project
-$lang['issuemod_changes']       = 'A ocorr&ecirc;ncia mudou em %s de %s para %s.'; //$column, $old_value, $new_value
+$lang['issuemod_end']           = ' Analisador de Ocorrências';    // project name placed before this
+$lang['issuedescrmod_subject']  = 'Ocorrência #%s em %s: descrição inicial modificada'; // $issue['id'], $project
+$lang['issuemod_changes']       = 'A ocorrência mudou em %s de %s para %s.'; //$column, $old_value, $new_value
 /******************************************************************************/
 /* send an e-mail to user due to issue set to resolved on details
 /* _emailForResolutionMod
 */                            
-$lang['issue_resolved_subject'] = 'A ocorr&ecirc;ncia %s em %s foi resolvida';
-$lang['issue_resolved_intro']   = 'A ocorr&ecirc;ncia relatada foi resolvida.';
+$lang['issue_resolved_subject'] = 'A ocorrência %s em %s foi resolvida';
+$lang['issue_resolved_intro']   = 'A ocorrência relatada foi resolvida.';
 $lang['issue_resolved_status']  = 'Resolvido';
-$lang['issue_resolved_text']    = 'Solu&ccedil;&atilde;o:   ';
-$lang['msg_resolution_true']    = 'Sua Solu&ccedil;&atilde;o foi adicionada com sucesso ao ID';
+$lang['issue_resolved_text']    = 'Solução:   ';
+$lang['msg_resolution_true']    = 'Sua Solução foi adicionada com sucesso ao ID';
 
 /******************************************************************************/
 /* deviations from before for send an e-mail to admin due to new issue created
 /* _emailForNewIssue
 */
-$lang['issuenew_subject']       = '%s ocorr&ecirc;ncia relatada para %s no Produto: %s (%s)';
+$lang['issuenew_subject']       = '%s ocorrência relatada para %s no Produto: %s (%s)';
 $lang['issuenew_head']          = 'Caro administrador,';
-$lang['issuenew_intro']         = 'uma nova ocorr&ecirc;ncia foi criada para o projeto:';
-$lang['issuenew_descr']         = 'Descri&ccedil;&atilde;o:  ';
+$lang['issuenew_intro']         = 'uma nova ocorrência foi criada para o projeto:';
+$lang['issuenew_descr']         = 'Descrição:  ';
 
 /******************************************************************************/
 /* deviations from before for sending an e-mail to assignee
 /* _emailForNewIssue
 */
-$lang['issueassigned_subject']  = ' Ocorr&ecirc;ncia %s atribu&iacute;da para voc&ecirc;';
-$lang['issueassigned_head']     = 'Ol&aacute;,';
-$lang['issueassigned_intro']    = 'foi-lhe atribu&iacute;da a seguinte Ocorr&ecirc;ncia:';
+$lang['issueassigned_subject']  = ' Ocorrência %s atribuída para você';
+$lang['issueassigned_head']     = 'Olá,';
+$lang['issueassigned_intro']    = 'foi-lhe atribuída a seguinte Ocorrência:';
 $lang['it__none']               = '--';
 /******************************************************************************/
 /* following text is related to search feature                                
@@ -168,10 +167,10 @@ $lang['lbl_search']             = 'Pesquisar pelo seguinte:&nbsp;&nbsp;';
 $lang['btn_search']             = 'Iniciar';
 $lang['btn_search_title']       = 'Pesquisar';
 $lang['search_hl1']             = '<h1>Pesquisar</h1>';
-$lang['search_txt1']            = "Voc&ecirc; pode ver abaixo os resultados da sua pesquisa. Lembre-se que esta &eacute; uma pesquisa simples que procura apenas por palavras e n&atilde;o entende l&oacute;gica de pesquisas.";
+$lang['search_txt1']            = "Você pode ver abaixo os resultados da sua pesquisa. Lembre-se que esta é uma pesquisa simples que procura apenas por palavras e não entende lógica de pesquisas.";
 $lang['search_hl2']             = '<h2>Resultado</h2>';
-$lang['search_Issue']           = 'Ocorr&ecirc;ncia';
-$lang['search_Comment']         = 'Coment&aacute;rio';
+$lang['search_Issue']           = 'Ocorrência';
+$lang['search_Comment']         = 'Comentário';
 $lang['search_Type']            = 'Tipo';
 $lang['search_ID']              = 'ID';
 $lang['search_Subject']         = 'Assunto';
--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Arquivo de idioma em Portugu�s do Brasil para o plugin IssueTracker
+ * Arquivo de idioma em Português do Brasil para o plugin IssueTracker
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Benedito Carneiro <ctsbc@yahoo.com.br>
@@ -8,31 +8,31 @@
  
 // for the configuration manager
 $lang['d_format']         = 'd-m-Y G:i:s';
-$lang['it_data']          = 'Raiz do IssueTracker na pasta de dados ("" = meta)<img title="Warning: It is very recommended to store the Issues outside the meta folder. So please enter a path here (e.g. \'data/it_store/\')." alt="warning" src="./lib/plugins/config/images/warning.png" style="float:right;"> ';
+$lang['it_data']          = 'Raiz do IssueTracker na pasta de dados ("" = meta)<img title="Aviso: Recomenda-se armazenar as ocorrências fora da pasta Meta. Então, por favor, entre um caminho (ex. \'data/it_store/\')." alt="aviso" src="./lib/plugins/config/images/warning.png" style="float:right;"> ';
 $lang['upload']           = 'Permitir upload de arquivos de sintomas?';
 $lang['max_fsize']        = 'Tamanho limite do arquivo para upload em Bytes';
 $lang['ip_blocked']       = 'Ligar recurso anti-spam?';
-$lang['ip_blockd_time']   = 'Por quanto tempo o IP ser&aacute; bloqueado antes que outro upload seja iniciado?';
-$lang['send_email']       = 'Informar por email sobre novas ocorr&ecirc;ncias?'; 
+$lang['ip_blockd_time']   = 'Por quanto tempo o IP será bloqueado antes que outro upload seja iniciado?';
+$lang['send_email']       = 'Informar por email sobre novas ocorrências?'; 
 $lang['mail_templates']   = 'Usar modelos de email em HTML?';
-$lang['email_address']    = 'Qual endere&ccedil;o de email deve ser informado?';
-$lang['registered_users'] = 'Somente usu&aacute;rios registrados com permiss&atilde;o de edi&ccedil;&atilde;o no registro de ocorr&ecirc;ncias<br> t&ecirc;m permiss&atilde;o de criar relat&oacute;rios e adicionar coment&aacute;rios?';
-$lang['auth_ad_overflow'] = 'Evitar sobrecarga AUTH:AD (ver documenta&ccedil;&atilde;o)?';
-$lang['userinfo_email']   = 'Informar por email o usu&aacute;rio sobre modifica&ccedil;&otilde;es em ocorr&ecirc;ncias? (tecla global)';
-$lang['mail_add_comment']         = 'Informar sobre novos coment&aacute;rios?';
-$lang['mail_modify_comment']      = 'Informar sobre modifica&ccedil;&otilde;es em coment&aacute;rios?';
-$lang['mail_add_resolution']      = 'Informar sobre entradas de solu&ccedil;&otilde;es?';
-$lang['mail_modify_resolution']   = 'Informar sobre modifica&ccedil;&otilde;es de solu&ccedil;&otilde;es?';
-$lang['mail_modify__description'] = 'Informar sobre modifica&ccedil;&otilde;es na descri&ccedil;&atilde;o inicial?';
-$lang['shw_mail_addr']  = 'Endere&ccedil;o de email vis&iacute;vel em vez do nome do usu&aacute;rio (apenas para usu&aacute;rios registrados)?';
-$lang['shw_assignee_as']= 'Identificar o usu&aacute;rio ao qual foi atribu&iacute;da a ocorr&ecirc;ncia por';
-$lang['shwtbl_usr']     = 'Colunas mostradas no relat&oacute;rio';
+$lang['email_address']    = 'Qual endereço de email deve ser informado?';
+$lang['registered_users'] = 'Somente usuários registrados com permissão de edição no registro de ocorrências<br> têm permissão de criar relatórios e adicionar comentários?';
+$lang['auth_ad_overflow'] = 'Evitar sobrecarga AUTH:AD (ver documentação)?';
+$lang['userinfo_email']   = 'Informar por email o usuário sobre modificações em ocorrências? (tecla global)';
+$lang['mail_add_comment']         = 'Informar sobre novos comentários?';
+$lang['mail_modify_comment']      = 'Informar sobre modificações em comentários?';
+$lang['mail_add_resolution']      = 'Informar sobre entradas de soluções?';
+$lang['mail_modify_resolution']   = 'Informar sobre modificações de soluções?';
+$lang['mail_modify__description'] = 'Informar sobre modificações na descrição inicial?';
+$lang['shw_mail_addr']  = 'Endereço de email visível em vez do nome do usuário (apenas para usuários registrados)?';
+$lang['shw_assignee_as']= 'Identificar o usuário ao qual foi atribuída a ocorrência por';
+$lang['shwtbl_usr']     = 'Colunas mostradas no relatório';
 $lang['use_captcha']    = 'Usar captcha?'; 
-$lang['severity']       = 'N&iacute;veis de severidade (separados por v&iacute;rgulas, dependem dos nomes dos arquivos de &iacute;cones)';
-$lang['status']         = 'Status de ocorr&ecirc;ncias (separados por v&iacute;rgulas, dependem dos nomes dos arquivos de &iacute;cones)';
-$lang['status_special'] = 'Ocorr&ecirc;ncias invis&iacute;veis (exclu&iacute;das) (apenas um valor de status &eacute; permitido!)';
-$lang['products']       = 'Produtos cobertos pelo Projeto (separados por v&iacute;rgulas)';
-$lang['assign']         = 'Selecionar grupos de usu&aacute;rios do wiki pr&eacute;-selecionados para atribuir ocorr&ecirc;ncias <br> (separados por pipe "|")';
-$lang['noStatIMG']      = 'Usar textos de status em lugar dos &iacute;cones na lista de ocorr&ecirc;ncias?';
-$lang['noSevIMG']       = 'Usar textos de severidade em lugar dos &iacute;cones na lista de ocorr&ecirc;ncias?';
-$lang['ltdReport']      = 'Excluir estes itens do formul&aacute;rio de relat&oacute;rio';
+$lang['severity']       = 'Níveis de severidade (separados por vírgulas, dependem dos nomes dos arquivos de ícones)';
+$lang['status']         = 'Status de ocorrências (separados por vírgulas, dependem dos nomes dos arquivos de ícones)';
+$lang['status_special'] = 'Ocorrências invisíveis (excluídas) (apenas um valor de status é permitido!)';
+$lang['products']       = 'Produtos cobertos pelo Projeto (separados por vírgulas)';
+$lang['assign']         = 'Selecionar grupos de usuários do wiki pré-selecionados para atribuir ocorrências <br> (separados por pipe "|")';
+$lang['noStatIMG']      = 'Usar textos de status em lugar dos ícones na lista de ocorrências?';
+$lang['noSevIMG']       = 'Usar textos de severidade em lugar dos ícones na lista de ocorrências?';
+$lang['ltdReport']      = 'Excluir estes itens do formulário de relatório';
